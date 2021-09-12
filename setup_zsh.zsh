38153c4d7d935119db7a894d88926664c69acf44
John <18251637+johnhodge@users.noreply.github.com>
2021-09-12 15:03:56 -0400

    add stdout redirection to dev/null

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -3,7 +3,7 @@
 echo "\n<<< Starting zsh setup >>>\n"
 
 echo "Enter to superuser password to edit the /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells'
+echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' > dev/null
 
 echo "Enter user password to change login shell"
 chsh -s '/usr/local/bin/zsh'
