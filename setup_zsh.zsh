3f3d9ccb0313ae248a5f58ee88604fcb28ec4878
John <18251637+johnhodge@users.noreply.github.com>
2022-02-18 11:22:21 -0500

    reword zsh set up to be less of an asshole

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,14 +4,14 @@ echo "\n############\nStarting zsh setup\n############\n"
 
 # https://stackoverflow.com/a/4749368/1341838
 if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
-  echo '/usr/local/bin/zsh already exists in /etc/shells'
+  echo '/usr/local/bin/zsh exists in /etc/shells'
 else
   # echo "Enter superuser (sudo) password to edit /etc/shells"
   echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
 
 if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
-  echo '$SHELL is already /usr/local/bin/zsh'
+  echo '$SHELL is /usr/local/bin/zsh'
 else
   echo "Enter user password to change login shell"
   chsh -s '/usr/local/bin/zsh'
