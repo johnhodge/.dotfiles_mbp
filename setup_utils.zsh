835c2e5f3bc6724c45e1cb43026369e20e2dac84
John <18251637+johnhodge@users.noreply.github.com>
2022-02-19 20:58:32 -0500

    add duti support

---
+++ setup_utils.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+echo "\n############\nStarting utils setup\n############\n"
+
+duti -sv com.microsoft.VSCode css all
+duti -sv com.microsoft.VSCode json all
+duti -sv com.microsoft.VSCode txt all
