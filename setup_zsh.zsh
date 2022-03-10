0c4b59f4937041876caaed47391d525c0818cf7d
John <18251637+johnhodge@users.noreply.github.com>
2022-03-10 11:39:46 -0500

    make zsh setup work on intel and m1

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -8,17 +8,23 @@ echo "${cyan}\n############
 ${light_magenta}${bold}Starting zsh setup
 ${reset}${cyan}############$reset\n"
 
-# https://stackoverflow.com/a/4749368/1341838
-if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
-  echo '/opt/homebrew/bin/zsh exists in /etc/shells'
+if [[ $(arch) == 'arm64' ]]; then
+  shell_path=/opt/homebrew/bin/zsh
 else
-  # echo "Enter superuser (sudo) password to edit /etc/shells"
-  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+  shell_path=/usr/local/bin/zsh
 fi
 
-if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
-  echo '$SHELL is /opt/homebrew/bin/zsh'
+# https://stackoverflow.com/a/4749368/1341838
+if grep -Fxq ${shell_path} '/etc/shells'; then
+  echo ${shell_path}' exists in /etc/shells'
+else
+  # echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo ${shell_path} | sudo tee -a '/etc/shells' >/dev/null
+fi
+
+if [ "$SHELL" = ${shell_path} ]; then
+  echo '$SHELL is '${shell_path} 
 else
   echo "Enter user password to change login shell"
-  chsh -s '/opt/homebrew/bin/zsh'
+  chsh -s ${shell_path} 
 fi
